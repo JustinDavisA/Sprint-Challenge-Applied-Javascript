87dc69c2d4a9a382e2c8d1fe8eb9094cee528cd3
justindavisa <justindavisa@gmail.com>
2019-02-01 17:35:50 -0500

    Added DOM references

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,48 @@
 class Carousel {
+    constructor(carouselElement){
+        // assign this.carouselElement to the carouselElement DOM reference
+        this.carouselElement = carouselElement;
 
+        // Get the `data-tab` value from this.carouselElement and store it here
+        this.carouselData = carouselElement.dataset;
+
+        // Select all images
+        this.imgs = document.querySelectorAll('.carousel img');
+
+        // Select right and left buttons
+        this.rgtBtn = document.getElementsByClassName('right-button');
+        this.lftBtn = document.getElementsByClassName('left-button');
+        
+        // Map over the newly converted NodeList
+        this.imgs = Array.from(this.imgs).map(imgs => new CarouselImg(imgs));
+        
+        // Add a click event that invokes this.rightClick
+        this.rgtBtn.addEventListener('click', () => this.rightClick());
+
+        // Add a click event that invokes this.leftClick
+        this.lftBtn.addEventListener('click', () => this.leftClick());
+    }
+
+    rightClick() {
+        this.rgtBtn.display = "none";
+    }
+
+    leftClick() {
+
+    }
 }
 
-let carousel = document.querySelector('.carousel').forEach( li => new TabLink(li));
+class CarouselImg {
+    constructor(imgElement) {
+        this.imgElement = imgElement;
+    } 
+
+    selectImg() {
+        this.imgElement.style.display = "flex";
+    }
+}
+
+let carousels = document.querySelectorAll('.carousel').forEach( caro => new Carousel(caro));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
